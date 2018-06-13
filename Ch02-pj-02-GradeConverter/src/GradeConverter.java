6f1635f2dfd05e86bdb0dd0cf0937e7d25e892cc
Princess Uwimpuhwe <tabithauwimpuhwe@gmail.com>
2018-06-13 15:46:24 -0400

    Grade Converter 2 added

--- Ch02-pj-02-GradeConverter/src/GradeConverter.java
+++ Ch02-pj-02-GradeConverter/src/GradeConverter.java
@@ -42,7 +42,6 @@ public class GradeConverter {
 		
 	System.out.println("Bye!");	
 	sc.close();
-		
 	
 	}
 
---
+++ Ch02-pj-02-GradeConverter/src/GradeConverter2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class GradeConverter2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Grade Converter App2");
+		Scanner sc = new Scanner(System.in);
+		
+		//declare your variables;
+		String lGrade=" "; 
+		String choice = "y";
+		
+	 While (choice.equalsIgnoreCase("y")); {
+	System.out.println("Enter Numeric Grade:");
+	int nGrade=sc.nextInt(); //enter numeric grade from user
+	
+		if (nGrade<60) {
+			lGrade="F";
+		}
+		else if (nGrade<68) {
+			lGrade ="D";
+		}
+		else if (nGrade<68) {
+			lGrade ="D";
+		}else if (nGrade<80) {
+			lGrade ="C";
+		}else if (nGrade<88) {
+			lGrade ="B";
+		}
+		else if (nGrade<=100) {
+			lGrade ="A";
+		}
+	System.out.println("Your Letter Grade is: " + lGrade);
+	System.out.println("Continue: y/n");	
+	choice=sc.next();
+	}
+	 System.out.println("Thank you for checking out our App!");
+		sc.close();
+	}
+
+	private static void While(boolean equalsIgnoreCase) {
+		// TODO Auto-generated method stub
+		
+	}
+}
