5c2e9d6c5e6903ff2e3a0cd436b8ea765f82432f
Princess Uwimpuhwe <tabithauwimpuhwe@gmail.com>
2018-06-15 13:07:49 -0400

    Finding Sum in An Array

---
+++ LoopingThruArray/src/FindSumInAnArray.java
@@ -0,0 +1,34 @@
+
+public class FindSumInAnArray {
+
+	public static void main(String[] args) {
+		
+		int [] array = {10,20,30,40,50} ;
+		
+		int sum = 0;
+		
+		for(int i = 0; i < array.length; i++){ 
+		    sum = sum+ array[i]; 
+		} 
+		//return sum;
+		System.out.println("The sum is " + sum);
+	}
+
+}
+
+
+//public class FindSumInAnArray {
+//
+//	public static int main(String[] args) {
+//		
+//		int [] array = null ;
+//		int sum = 0;
+//		
+//		for(int i = 0; i < array.length; i++){ 
+//		    sum = sum+ array[i]; 
+//		} 
+//		return sum;
+//	
+//	}
+//
+//}
